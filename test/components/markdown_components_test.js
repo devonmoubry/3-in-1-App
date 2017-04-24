9b52184dd575cfbd4a5a80161e1951b894890f93
Devon Moubry <devon@moubry.com>
2017-04-23 20:38:28 -0500

    prettier

--- test/components/markdown_components_test.js
+++ test/components/markdown_components_test.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { MemoryRouter } from "react-router-dom";
 import { shallow, mount, render } from "enzyme";
-import { Provider } from "react-redux"
+import { Provider } from "react-redux";
 
-import MarkdownComponents from "../../app/scripts/components/editor_components/markdown_components.js";
+import MarkdownComponents
+  from "../../app/scripts/components/editor_components/markdown_components.js";
 
 // How to test components using Redux:
 // http://www.wsbrunson.com/react/redux/test/2016/05/08/testing-redux-containers.html
 
-const createFakeStore = (state) => {
+const createFakeStore = state => {
   return {
     default: () => {},
     subscribe: () => {},
@@ -21,15 +22,19 @@ const createFakeStore = (state) => {
 
 describe("Markdown Components", () => {
   const store = createFakeStore({
-    markdownPreview: 'Haha',
-    markdownNotes: '',
+    markdownPreview: "Haha",
+    markdownNotes: "",
     showConfirmationMessage: false
   });
 
   let Component;
 
   beforeEach(() => {
-    Component = render(<Provider store={store}><MarkdownComponents /></Provider>);
+    Component = render(
+      <Provider store={store}>
+        <MarkdownComponents />
+      </Provider>
+    );
   });
 
   it("should have a title", () => {
@@ -37,6 +42,6 @@ describe("Markdown Components", () => {
   });
 
   it("should populate preview", () => {
-    expect(Component.find('.textarea').text()).toContain("Haha");
+    expect(Component.find(".textarea").text()).toContain("Haha");
   });
 });
