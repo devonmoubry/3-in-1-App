7e45fe7493c6688f11899e9ec5c02c8f8d401198
Devon Moubry <devon@moubry.com>
2017-04-23 21:14:27 -0500

    create file for api test

---
+++ test/reducers/get_questions_api_test.js
@@ -0,0 +1,6 @@
+import reducer from "../../app/scripts/reducers/app_reducer.js";
+import { assert } from "chai";
+
+describe ("question action", () => {
+  it("should call the ajax request on jQuery object")
+})
